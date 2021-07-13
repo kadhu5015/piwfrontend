0448d205ba4c6683849dd785d0aa2600552f6aa9
Eduardo Almeida <eduardoalmeida8246@gmail.com>
2021-07-12 23:02:58 -0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+//import { PaginaPrincipal } from './components/pages/PaginaPrincipal/PaginaPrincipal';
+import { PaginaPrincipal } from './components/pages/PaginaPrincipal/PaginaPrincipal';
 
-function Link(){
-
-    return <a href="https://www.google.com.br">link aqui mlr</a>
-
-}
-
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          inhaim vamu PIWzar? puttttttz <Link></Link>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <PaginaPrincipal></PaginaPrincipal> 
   );
 }
 
---
+++ src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -0,0 +1,15 @@
+
+export function PaginaPrincipal(){
+
+    return (<div>
+        <nav>
+        <a>link 1</a>
+        <a>link 2</a>
+        <a>link 3</a>
+        </nav>
+        <div>
+            <p>meu conteudo</p>
+        </div>
+    </div>) 
+
+}
