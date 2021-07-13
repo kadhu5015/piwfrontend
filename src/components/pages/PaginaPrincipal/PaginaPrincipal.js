85a2b99d9d515940fdca46707bf15d8f5827b8f6
Eduardo Almeida <eduardoalmeida8246@gmail.com>
2021-07-13 14:22:37 -0300

    atualizando

--- src/components/pages/PaginaPrincipal/PaginaPrincipal.js
+++ src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,7 +1,5 @@
 import { Navegador } from "../../common/Navegador/Navegador";
-//alteração teste para ver o log
-//agora vamos ver o universo paralelo
-//agora outro teste
+
 function Conteudo(){
     let disciplinas = [ //isso é pra simular informações que estão no banco de dados
         {
