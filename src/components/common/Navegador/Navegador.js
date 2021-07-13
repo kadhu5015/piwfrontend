4173b61c507d3d32047ad41e17466fbf294fad3e
Eduardo Almeida <eduardoalmeida8246@gmail.com>
2021-07-13 12:30:23 -0300

    atualizacao aula 02 t04

---
+++ src/components/common/Navegador/Navegador.js
@@ -0,0 +1,19 @@
+//Como estava antes:
+// export function Navegador(){
+//    return ( <nav>
+//         <a>link 1</a>
+//         <a>link 2</a>
+//         <a>link 3</a>
+//         </nav>); 
+// } 
+export function Navegador(){
+    //nao entendi essa linha do let Link
+    let Link = ({linkTexto}) =>(<a className="link-navegador" href="http://globo.com">{(linkTexto)}</a>) //esse é o componentezinho que o proff fez no min 11:46
+    return (
+        <nav>
+            <Link linkTexto = "link 1 "></Link> 
+            <Link linkTexto = "link 2 "></Link> 
+            <Link linkTexto = "link 3 "></Link> 
+        </nav>
+    );
+}
--- src/components/pages/PaginaPrincipal/PaginaPrincipal.js
+++ src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,15 +1,26 @@
+import { Navegador } from "../../common/Navegador/Navegador";
+
+function Conteudo(){
+    return(<div>
+        <p>meu conteudo atualizado</p>
+    </div>);
+}
+
+function Cabecalho({paginaAtual}){ //aqui eu criei uma variavel chamada paginaAtual que vai receber o valor da chave "paginaAtual" pq tem o mesmo nome
+    return( //não esquecer o return
+        <header>
+            <h1>Matricula Ufc QXD</h1>
+            <span>Você está na {paginaAtual}</span>
+        </header>
+    )
+}
 
 export function PaginaPrincipal(){
-
+    let nome = "Eduardo";
+    let link = (<a href="http://globo.com">Link para a globo</a>);// na tag <Cabeçalho> temos "paginaAtual="p de matricula", a paginaAtual é a chave e o outro o valor"
     return (<div>
-        <nav>
-        <a>link 1</a>
-        <a>link 2</a>
-        <a>link 3</a>
-        </nav>
-        <div>
-            <p>meu conteudo</p>
-        </div>
-    </div>) 
-
+        <Cabecalho paginaAtual="Página principal"></Cabecalho> 
+        <Navegador></Navegador>
+        <Conteudo></Conteudo>
+    </div>); 
 }
