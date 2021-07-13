7dbeaa5615273c8257e28d136143b69cdd80d682
Eduardo Almeida <eduardoalmeida8246@gmail.com>
2021-07-13 12:56:14 -0300

    atualizacao aula 09 t 04

--- src/components/pages/PaginaPrincipal/PaginaPrincipal.js
+++ src/components/pages/PaginaPrincipal/PaginaPrincipal.js
@@ -1,9 +1,36 @@
 import { Navegador } from "../../common/Navegador/Navegador";
 
 function Conteudo(){
-    return(<div>
-        <p>meu conteudo atualizado</p>
-    </div>);
+    let disciplinas = [ //isso é pra simular informações que estão no banco de dados
+        {
+            nome: "LMS",
+            codigo: "QXD123",
+
+        }, 
+        {
+            nome: "piw",
+            codigo: "QXD456",
+        },
+        {
+            nome: "sct",
+            codigo: "QXD789",
+        }
+
+    ];
+    // let lis = []; //crio uma lista vazia pra armazenar os dados la de cima
+    // for(let i = 0; i < disciplinas.length; i++){ //um for que vai passar por todos os objetos dentro de disciplinas
+    //     lis.push(<li>({disciplinas[i].nome} - {disciplinas[i].codigo})</li>) //pegando todos os elementos das disciplinas e colocando dentro de lis
+    // }
+    // //no return a gente renderiza essa lista
+    let lis = disciplinas.map((disciplina)=>(
+        <li>
+            {disciplina.nome} - {disciplina.codigo}
+        </li>))
+
+    return(
+        <div>
+            {lis} 
+        </div>);
 }
 
 function Cabecalho({paginaAtual}){ //aqui eu criei uma variavel chamada paginaAtual que vai receber o valor da chave "paginaAtual" pq tem o mesmo nome
