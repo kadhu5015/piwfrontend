41359326b3a71ae77c8e35777bc0b3a962d30232
Eduardo Almeida <eduardoalmeida8246@gmail.com>
2021-07-16 13:48:22 -0300

    at finalizada

--- src/components/common/Navegador/Navegador.js
+++ src/components/common/Navegador/Navegador.js
@@ -1,11 +1,12 @@
 import "./Navegador.css";
+import imagemtt from "./tt.jpg";
 
 export function Navegador(){
  
     return (
         <div className="navegador">
 
-            <img src="tt.jpg"></img> 
+            <img src={imagemtt} className="imagem"></img> 
             <button className="linhadotempo" alt="logo">Linha do tempo</button>
             <button className="postar">Postar</button>
             <div className="logado">Eduardo (online)</div>
